5a29ec54073f8a91d8a119cc5a38d5aeb2bcb9a6
Rodrigo mendez <rodrigo_2392@hotmail.com>
2020-09-07 13:58:48 -0500

    FC

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,82 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState, useEffect} from 'react';
 import './App.css';
 
+interface Turno {
+  nombre: string;
+  proyecto: string;
+}
+
+const getTurnos = async(cb:any)=> {
+  fetch('http://localhost:3000/api/turnos')
+  .then(res=>res.json())
+  .then(res=>{
+    cb(res)
+  });
+}
+
+const agregarTurno = async(cb:any, data:Turno)=> {
+  fetch('http://localhost:3000/api/turnos',{
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(data)
+  })
+  .then(res=>res.json())
+  .then(res=>{
+    cb(res)
+  })
+}
+
 function App() {
+  
+  const [nombre, setNombre] = useState('')
+  const [proyecto, setProyecto] = useState('')
+  const [turnos, setTurnos] = useState<Turno[]>([])
+  
+  const save=(evt:any)=>{
+    evt.preventDefault();
+
+    const turno = {
+      nombre, proyecto
+    }
+    console.log(turno)
+   // agregarTurno(setTurnos, turno);
+  }
+
+  useEffect(()=>{
+    getTurnos(setTurnos)
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1 className="title">Lista de espera</h1>
+      <form onSubmit={save}>
+        <div>
+          <input name="proyecto" type="text" onChange={evt=>setNombre(evt.target.value)} placeholder="Nombre" />
+        </div>
+        <div>
+          <input name="nombre" type="text" onChange={evt=>setProyecto(evt.target.value)} placeholder="Proyecto" />  
+        </div>
+
+        <div>
+          <input type="submit" value="Agregar" />
+        </div>
+        
+       
+      </form>
+      <div>
+        <ul>
+          {
+            turnos.map((turno:any) =>(
+              <li key={turno.id}>
+                {turno.name} - {turno.project}
+              </li>
+            ))
+          }
+        </ul>
+      </div>
+    
     </div>
   );
 }
