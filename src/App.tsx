a099af9a4ca0341c1808ee54b17d480d9c3d855c
Rodrigo Mendez <rodrigomendez@Conferences-MacBook-Pro.local>
2020-09-12 21:49:35 -0500

    test

--- src/App.tsx
+++ src/App.tsx
@@ -2,20 +2,23 @@ import React, {useState, useEffect} from 'react';
 import './App.css';
 
 interface Turno {
-  nombre: string;
-  proyecto: string;
+  id?: number;
+  name: string;
+  project: string;
 }
 
-const getTurnos = async(cb:any)=> {
-  fetch('http://localhost:3000/api/turnos')
+const getTurnos = async(cb:any, setProject:any, setName:any)=> {
+  fetch('http://157.245.117.205:3000/api/turnos')
   .then(res=>res.json())
   .then(res=>{
     cb(res)
+    setProject("")
+    setName("")
   });
 }
 
 const agregarTurno = async(cb:any, data:Turno)=> {
-  fetch('http://localhost:3000/api/turnos',{
+  fetch('http://157.245.117.205:3000/api/turnos',{
     method: 'POST',
     headers: {
       'Content-Type': 'application/json'
@@ -27,56 +30,91 @@ const agregarTurno = async(cb:any, data:Turno)=> {
     cb(res)
   })
 }
+const removeTurno = async (cb: any, setProject: any, setName: any, data: any) => {
+  fetch('http://157.245.117.205:3000/api/finish',{
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(data)
+  })
+  .then(res=>res.json())
+  .then(res=>{
+    getTurnos(cb, setProject, setName);
+  })
+}
 
 function App() {
   
-  const [nombre, setNombre] = useState('')
-  const [proyecto, setProyecto] = useState('')
+  const [name, setName] = useState('')
+  const [project, setProject] = useState('')
   const [turnos, setTurnos] = useState<Turno[]>([])
   
   const save=(evt:any)=>{
     evt.preventDefault();
 
-    const turno = {
-      nombre, proyecto
+    if(name==="" || project==="")
+    {
+      return alert("Debes ingresar el nombre de la persona y el proyecto")
     }
-    console.log(turno)
-   // agregarTurno(setTurnos, turno);
+    const turno = {
+      name, project
+    }
+   agregarTurno(setTurnos, turno);
+  }
+
+  const remove = (id:number) => {
+    const data = {
+      id,
+      finished: true
+    }
+    removeTurno(setTurnos, setProject, setName, data);
   }
 
   useEffect(()=>{
-    getTurnos(setTurnos)
+    getTurnos(setTurnos, setProject, setName)
   }, []);
 
   return (
     <div className="App">
-      <h1 className="title">Lista de espera</h1>
-      <form onSubmit={save}>
+      <h1 className="main-title">Lista de espera</h1>
+      <form onSubmit={save} className="form">
         <div>
-          <input name="proyecto" type="text" onChange={evt=>setNombre(evt.target.value)} placeholder="Nombre" />
+          <input style={{color: "#FFF"}} name="proyecto" type="text" onChange={evt=>setName(evt.target.value)} placeholder="Nombre" />
         </div>
         <div>
-          <input name="nombre" type="text" onChange={evt=>setProyecto(evt.target.value)} placeholder="Proyecto" />  
+          <input style={{color: "#FFF"}} name="nombre" type="text" onChange={evt=>setProject(evt.target.value)} placeholder="Proyecto" />  
         </div>
 
         <div>
-          <input type="submit" value="Agregar" />
+          <input className="btn" type="submit" value="Agregar" />
         </div>
         
        
       </form>
       <div>
-        <ul>
-          {
-            turnos.map((turno:any) =>(
-              <li key={turno.id}>
-                {turno.name} - {turno.project}
-              </li>
-            ))
-          }
-        </ul>
+        <div className="container">
+          <div className="card">
+            <ul className="list">
+              {
+                turnos.map((turno:any) =>(
+                  <li style={{marginBottom: 20}}>
+                    <div className="valign-wrapper">
+                      <h1 style={{margin: '0 10px 0 0', fontSize: '3rem'}}>{turno.number}</h1>
+                      <div className="title">
+                        {turno.name} - {turno.project}<br />
+                        <span>Jan 9, 2017</span>
+                      </div>
+                      <button className="button" onClick={()=>remove(turno.id)}> <i className="material-icons ml-auto waves-effect" style={{width: 'auto'}}>close</i></button>
+                    </div>
+                  </li>
+                ))
+              }
+              
+            </ul>
+          </div>
+        </div>
       </div>
-    
     </div>
   );
 }
